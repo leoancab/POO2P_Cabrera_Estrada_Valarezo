f5a18f86f96b517f2b84c9b316311ad11e0112cb
unknown <leoancab@espol.edu.ec>
2022-08-24 18:19:45 -0500

    Arreglo de errores

---
+++ Proyecto2P/src/main/java/com/mycompany/proyecto2p/DetalleEquiposController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto2p;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author leoan
+ */
+public class DetalleEquiposController implements Initializable{
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+    
+}
